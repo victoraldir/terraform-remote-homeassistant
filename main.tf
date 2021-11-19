bc73ca6484de1d82a4ed9ab68a99913041b46158
vimontes <victor.montes@mercadolivre.com>
2021-11-19 15:09:59 -0300

    Add user data that configure nginx and enable https using certbot and duckdns

--- main.tf
+++ main.tf
@@ -38,6 +38,30 @@ resource "aws_security_group_rule" "ingress_ssh_host" {
   security_group_id = aws_security_group.bastion_host_security_group.id
 }
 
+resource "aws_security_group_rule" "ingress_http" {
+
+  description = "HTTP traffic to bastion"
+  type        = "ingress"
+  from_port   = "80"
+  to_port     = "80"
+  protocol    = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.bastion_host_security_group.id
+}
+
+resource "aws_security_group_rule" "ingress_https" {
+
+  description = "HTTPS traffic to bastion"
+  type        = "ingress"
+  from_port   = "443"
+  to_port     = "443"
+  protocol    = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.bastion_host_security_group.id
+}
+
 resource "aws_security_group_rule" "egress_host" {
   description = "Outgoing traffic from bastion to instances"
   type        = "egress"
@@ -50,19 +74,55 @@ resource "aws_security_group_rule" "egress_host" {
 }
 
 
+data "template_file" "nginx_config" {
+  template = file("${path.module}/template/nginx.config.tpl")
+  vars = {
+    SUBDOMAIN = "${var.subdomain}.duckdns.org"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon-linux-2.id
   instance_type = "t3.micro"
   key_name      = aws_key_pair.ha-tunneling.key_name
   security_groups = [aws_security_group.bastion_host_security_group.name]
-  user_data = <<-EOF
+  user_data = <<-REALEND
                     #!/bin/bash
+                    exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+                    echo 'Installing nginx..'
                     sudo yum update -y
-                    sudo yum install nginx -y
+                    sudo amazon-linux-extras install nginx1 -y
                     sudo service nginx start
-                EOF
+
+                    echo 'Installing certbot'
+                    sudo amazon-linux-extras install epel -y
+                    sudo yum install certbot-nginx -y
+
+                    echo 'Setting IP for subdomain'
+                    #PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+                    PUBLIC_IP=${aws_eip.lb.public_ip}
+                    echo 'Public IP is '$PUBLIC_IP
+                    echo 'URL: https://www.duckdns.org/update/${var.subdomain}/${var.duckdns_token}/'$PUBLIC_IP
+                    curl --location --request GET 'https://www.duckdns.org/update/${var.subdomain}/${var.duckdns_token}/'$PUBLIC_IP
+                    echo 'Installing certificate'
+                    cat << EOF > /tmp/nginx.conf
+                    ${data.template_file.nginx_config.rendered}
+                    EOF
+                    sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bkp
+                    sudo cp /tmp/nginx.conf /etc/nginx/nginx.conf
+                    sudo certbot --nginx -m ${var.email} -d ${var.subdomain}.duckdns.org --agree-tos -n
+                REALEND
 
   tags = {
-    Name = "HelloWorld"
+    Name = "proxy-home-assistant"
   }
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.lb.id
+}
+
+resource "aws_eip" "lb" {
+  vpc      = true
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "aws_region" {
   type = string
 }
+
+variable "subdomain" {
+  type = string
+}
+
+variable "email" {
+  type = string
+}
+
+variable "duckdns_token" {
+  type = string
+}
